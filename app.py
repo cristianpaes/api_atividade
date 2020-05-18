bfcd210bb9148a3759ac0fe349771f06e2e33919
cristianpaes <s4camargo@gmail.com>
2020-05-18 18:21:18 -0300

    Alterando nossa REST API para realizar autenticação no BD. Em models foi criado a tabela de usuarios com login e senha para autenticação nas APIs. Incluído em requirements a extensão flask-httpauth.

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask_restful import Resource, Api,request
-from models import Pessoas,Atividades
+from models import Pessoas,Atividades,Usuarios
+from flask_httpauth import HTTPBasicAuth
 
+auth = HTTPBasicAuth()
 app = Flask(__name__)
 api = Api(app)
 
+#USUARIOS = {
+#    'Cristian':'123',
+#    'Carlos':'123'
+#}
+
+#@auth.verify_password
+#def verificacao(login, senha):
+#    if not (login, senha):
+#        return False
+#    return USUARIOS.get(login) == senha
+
+@auth.verify_password
+def verificacao(login, senha):
+    if not (login, senha):
+        return False
+    return Usuarios.query.filter_by( login=login, senha=senha).first()
+
 class Pessoa(Resource):
+    @auth.login_required
     def get(self, nome):
         pessoa = Pessoas.query.filter_by(nome=nome).first()
         try:
@@ -43,6 +63,7 @@ class Pessoa(Resource):
         return {'status':'sucesso', 'mensagem':mensagem}
 
 class LstaPessoas(Resource):
+    @auth.login_required
     def get(self):
         pessoas = Pessoas.query.all()
         response = [{'id':i.id, 'nome':i.nome, 'idade':i.idade} for i in pessoas]
@@ -60,6 +81,7 @@ class LstaPessoas(Resource):
         return response
 
 class ListaAtividades(Resource):
+    @auth.login_required
     def get(self):
         atividades = Atividades.query.all()
         response = [{'id':i.id, 'nome':i.nome, 'pessoa':i.pessoa.nome} for i in atividades]
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 # Insere dados na tabela
 def insere_pessoas():
@@ -24,8 +24,21 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+# Insere dados na tabela Usuarios
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+# Consulta dados na tabela Usuarios
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
 if __name__ == '__main__':
     #insere_pessoas()
     #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    #exclui_pessoa()
+    #consulta_pessoas()
+    insere_usuario('Cristian', '123')
+    insere_usuario('Rafael', '4321')
+    consulta_todos_usuarios()
